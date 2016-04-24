adfa1b8c1169c3a7b2a3053f918672c1017fb845
Zoltan Biber <zoltan.biber@gmail.com>
2016-04-24 16:33:20 +0100

    Basic csv import works. User object is created for each row without validations.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,26 @@
 class UsersController < ApplicationController
+	before_action :check_file_format, only: [:import]
+
   def index
+    @users = User.all
   end
 
   def import
+		begin
+			User.import(params[:file])
+			redirect_to users_path, notice: 'CSV imported successfully!'
+		rescue
+			redirect_to root_path, notice: 'An error occured during the import, please check if the data in the CSV file is in the right format!'
+		end
+  end
+
+  private
+
+  def check_file_format
+		if params[:file].content_type == 'text/csv'
+			true
+		else
+			redirect_to root_path, notice: "The file you are trying to upload is not a csv file!"
+		end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,20 @@
 class User < ActiveRecord::Base
+	require 'csv'
+
+	def self.import(file)
+		CSV.foreach(file.path, headers: true) do |row|
+			User.create(	unique_code: row.fields[0],
+										relationship_to_child: row.fields[1] ? row.fields[1] : row.fields[2],
+										child_age: row.fields[3],
+										purchase_occasion: row.fields[4] ? row.fields[4] : row.fields[5],
+										gender: row.fields[6],
+										age: row.fields[7],
+										has_children: row.fields[8],
+										email: row.fields[9],
+										started_survey_at: row.fields[10],
+										submitted_survey_at: row.fields[11],
+										network_id: row.fields[12]
+									)
+		end
+	end
 end
